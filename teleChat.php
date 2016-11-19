f6a84b43b85454eb93c7123752fbd937926e7ee6
OheneAppiah <ohene.sekyi@st.vvu.edu.gh>
2016-11-18 20:51:30 -0800

    Add files via upload
    This file uploaded is to communicate directly with the bot. Example when you send a message 'hi' the bot should automatically reply to you a message. This codes was created today.

--- teleChat.php
+++ teleChat.php
@@ -1,9 +1,13 @@
 <?php
+
+
 $botToken = "261124504:AAGrB9eq84cA57Mr9hytoDzr1NcY2qcoIDk";
 $website = "https://api.telegram.org/bot".$botToken;
 
-$update = file_get_contents($website."/get updates");
-print_r($update);
-
 ?>
 
+<form
+action="<?php echo $website.'/ sendHi' ?> method= "post" enctype = "multipart/form-data">
+<input type= "text" name"chat_id" value="280657512" />
+
+</form>
